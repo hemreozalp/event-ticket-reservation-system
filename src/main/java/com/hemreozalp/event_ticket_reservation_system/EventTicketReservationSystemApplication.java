6902150f309d8d5b01050db5f1bbcfb64e590f4a
hemreozalp <h.emreozalpp@gmail.com>
2026-06-30 18:30:35 +0300

    initial commit

---
+++ src/main/java/com/hemreozalp/event_ticket_reservation_system/EventTicketReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hemreozalp.event_ticket_reservation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicketReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventTicketReservationSystemApplication.class, args);
+	}
+
+}
