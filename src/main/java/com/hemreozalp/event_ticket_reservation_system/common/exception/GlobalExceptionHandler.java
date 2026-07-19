bf2eba007509e21cac5b93455fa14946a71ec891
hemreozalp <h.emreozalpp@gmail.com>
2026-07-19 19:53:37 +0300

    feat(exception): add exception for optimistic locking

--- src/main/java/com/hemreozalp/event_ticket_reservation_system/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hemreozalp/event_ticket_reservation_system/common/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.hemreozalp.event_ticket_reservation_system.common.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -24,6 +25,22 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity
                 .badRequest()
+                .body(body);
+    }
+
+    @ExceptionHandler(ObjectOptimisticLockingFailureException.class)
+    public ResponseEntity<Map<String, Object>> handleOptimisticLocking(
+            ObjectOptimisticLockingFailureException ex) {
+
+        Map<String, Object> body = new HashMap<>();
+
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.CONFLICT.value());
+        body.put("error", HttpStatus.CONFLICT.name());
+        body.put("message", "Seat has already been reserved by another user. Please try again.");
+
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
                 .body(body);
     }
 }
